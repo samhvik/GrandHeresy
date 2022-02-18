68edfc831847c8b5d7d5c2880562467c85e34587
Kevin <kchacko@ucsc.edu>
2022-02-18 10:39:56 -0800

    checking if it ignores .dwlt

--- Assets/ObjectiveController.cs
+++ Assets/ObjectiveController.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class ObjectiveController : MonoBehaviour
 {
     // Start is called before the first frame update
-   
+    //GameValues.instance.objectivesTotal;
+    // GameValues.instance.objectivesCompleted;
     public GameObject[] spawnsSW;
     public GameObject[] spawnsSE;
     public GameObject[] spawnsNE;
     public GameObject[] spawnsNW;
+    public GameObject[] mapWideSpawns;
     public GameObject[] chosenSpawns;
     //should be set within inspector to some gameobject (default is a tree)
     public GameObject objective1ToSpawn;
@@ -17,15 +19,21 @@ public class ObjectiveController : MonoBehaviour
     public GameObject[] objectivesToSpawn;
     private GameObject objectSpawned;
     private List<GameObject> spawnedObjects = new List<GameObject>();
+    private List<int> spawnChoices = new List<int>();
     void Awake()
     {
         //objective1ToSpawn = (GameObject)Resources.Load("name of prefab to spawn", typeof(GameObject));
         //why we have to use resources folder: https://answers.unity.com/questions/1170405/resourcesload-without-a-resources-folder.html
-       objective1ToSpawn = (GameObject)Resources.Load("Tree",typeof(GameObject));
+       if(objective1ToSpawn == null){
+           objective1ToSpawn = (GameObject)Resources.Load("Tree",typeof(GameObject));
+        }
+    //    GameValues.instance.objectiveCompleted = 4;
+       chosenSpawns = chooseSpawnsMapWide(4);
        //choose spawn groups
-       chosenSpawns = chooseSpawnGroup();
+    //    chosenSpawns = chooseSpawnGroup();
        //go through chosen spawns and instantiate at each point 
        //currently only handles spawning one objective type
+
        foreach (GameObject spawnPoint in chosenSpawns){
            objectSpawned = GameObject.Instantiate(objective1ToSpawn, spawnPoint.transform.position, Quaternion.identity);
            spawnedObjects.Add(objectSpawned);
@@ -35,6 +43,7 @@ public class ObjectiveController : MonoBehaviour
            print(spawnPoint.name);
        }
        /*
+
         //picks a random objective from a list of objectives to spawn at a specified spawn point 
         if(objectiveToSpawn.Length >= 2){
             foreach(GameObject spawnPoint in chosenSpawns){
@@ -104,6 +113,30 @@ public class ObjectiveController : MonoBehaviour
             default: 
                 return spawnsSW;
         }
+    }
+
+    GameObject[] chooseSpawnsMapWide(int numObjs){
+       mapWideSpawns = GameObject.FindGameObjectsWithTag("ObjectiveSpawn"); 
+       int randSpawn = 0; 
+       int j =0;
+       GameObject[] spawns = new GameObject[numObjs];
+       print(mapWideSpawns.Length + "mapWideSpawns");
+       while(spawnChoices.Count < numObjs){
+           randSpawn = Random.Range(0,12);
+           if(!spawnChoices.Contains(randSpawn)){
+               spawnChoices.Add(randSpawn);
+           }
+       }
+       foreach( int spawnInd in spawnChoices){
+           print(spawnInd + "spawnInd");
+           spawns[j] = mapWideSpawns[spawnInd];
+        //    print(mapWideSpawns[spawnInd].name + " added to spawns");
+           j++;
+       }
+       foreach(GameObject spawnPoint in spawns){
+           print("sanity check " + spawnPoint.name);
+       }
+       return spawns;
     }
     public GameObject[] getChosenSpawn(){
         if(chosenSpawns != null){
