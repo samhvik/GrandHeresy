0d0872cdcf24d2a4eb02d1b024f6e5ec82bd379a
LaneAlb <LaneT.Albrecht@gmail.com>
2022-04-12 14:22:58 -0700

    Logic comments Range AI update

--- Assets/Scripts/AI/AISpecialAttack.cs
+++ Assets/Scripts/AI/AISpecialAttack.cs
@@ -9,16 +9,26 @@ public class AISpecialAttack: AIAction
         Attack(controller);
     }
 
+    private void Special(AIStateController c){
+
+    }
+
     private void Attack(AIStateController controller){
         AIFOV fov = controller.GetComponent<AIFOV>();
         if(fov == null) return;
 
         if(fov.visibleTarget != null && fov.visibleTarget.CompareTag("Player")){ // visible target found
             // do special attack IF possible
-            // if controller.enemyStats.SpecialCD != 0 && controller.CheckIfCountdownElapse(controller.enemyStats.SpecialCD)
-            // enemy STOPS moving for the entire attack
-            // controller.StartCoroutine(attackingPause(controller));
-            // then do the attack here
+            if(controller.enemyStats.SpecialCD != 0 && controller.CheckIfCountdownElapse(controller.enemyStats.SpecialCD)){
+                controller.navMeshAgent.isStopped = true;
+                //Debug.Log("Attack Animation Here");
+                // Projector / Draw Circle on the ground
+                // Debug.Log("Attack Sound Here");
+                // Cast time later just get it to work
+
+                // update the players health
+                fov.visibleTarget.gameObject.GetComponent<PlayerInventory>().UpdateHealth(controller.enemyStats.damage);
+            }
 
             // basic attack
             if(controller.CheckIfCountdownElapse(controller.enemyStats.attackCD) && controller.checkRange()){
