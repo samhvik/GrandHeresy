d7993cd6206cb6a238a1af693a5652591075301c
Lane Albrecht <LaneT.Albrecht@gmail.com>
2022-04-15 00:37:44 -0700

    Range Enemy AoE particle system.
    Should get spawning and attacking tomorrow

--- Assets/Scripts/AI/AISpecialAttack.cs
+++ Assets/Scripts/AI/AISpecialAttack.cs
@@ -6,13 +6,9 @@ using UnityEngine;
 public class AISpecialAttack: AIAction
 {
     [SerializeField] private LayerMask PlayerLayer;
-    [SerializeField] private ParticleSystem GroundEffect;
+    [SerializeField] private ParticleSystem AoEParticleSet;
     public override void Act(AIStateController controller){
         Attack(controller);
-    }
-
-    private void Special(AIStateController c){
-
     }
 
     private void Attack(AIStateController controller){
@@ -24,13 +20,13 @@ public class AISpecialAttack: AIAction
             // controller.enemyStats.specialCD != 0 && 
             if(controller.CheckIfCountdownElapse(controller.enemyStats.specialCD)){
                 controller.navMeshAgent.isStopped = true;
-                Debug.Log("Starting to Cast...");
+                //Debug.Log("Starting to Cast...");
                 // update the players health & attack
                 controller.StartCoroutine(attackingPause(fov.visibleTarget.gameObject, controller));
                 controller.stateTimeElapsed = -controller.enemyStats.specialCD; // reset time elapsed
             }
 
-            /* basic attack
+            /* basic attack?
             if(controller.CheckIfCountdownElapse(controller.enemyStats.attackCD) && controller.checkRange()){
                 //Stop Controller from Moving briefly for animations
                 controller.navMeshAgent.isStopped = true;
@@ -44,18 +40,24 @@ public class AISpecialAttack: AIAction
     }
 
     IEnumerator attackingPause(GameObject tar, AIStateController c){
+        // instantiate particle system
+        // GameObject AoE = Instantiate(AoEParticleSet, tar.transform.position, Quaternion.identity);
+        Destroy(Instantiate(AoEParticleSet, tar.transform.position, Quaternion.identity), 1); // destroy after cast
         yield return new WaitForSeconds(0.5f);
         Debug.Log("Attack Animation Here && Draw Circle on Ground");
         // only hit players and update health
         Collider[] hits = Physics.OverlapSphere(tar.transform.position, 3, PlayerLayer);
-        foreach(Collider h in hits){ h.GetComponent<PlayerInventory>().UpdateHealth(c.enemyStats.damage);  }
+        foreach(Collider h in hits){ h.GetComponent<PlayerInventory>().UpdateHealth(c.enemyStats.damage);  
+        Debug.Log("Hit: " + h.GetComponent<PlayerInventory>().health);}
 
         yield return new WaitForSeconds(0.25f);
+        // destroy particle system
+        // Destroy(AoE);
         c.navMeshAgent.isStopped = false; // let the AI walk again
     }
 }
 
-// Ranged Attack Issues
+// Projectile Ranged Attack Issues
 // using a bullet since the script is already made, altering the "round" script to detect when a bullet hits a "Player"
 // would induce friendly fire into the game. a way around that?
 // IDEAS
