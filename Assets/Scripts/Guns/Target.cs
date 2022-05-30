46f080cdb0cc6a8e6c956a2c6cfea6d33dbe9cdf
Josue Armando Uriarte Reyes <47550290+JosueUriarte@users.noreply.github.com>
2022-05-30 02:10:08 -0700

    enemies dissolve when dying

--- Assets/Scripts/Guns/Target.cs
+++ Assets/Scripts/Guns/Target.cs
@@ -4,23 +4,39 @@
     Allows for rounds fired from a gun to damage the gameObject.
 */
 
-using System;
 using UnityEngine;
 using UnityEngine.AI;
+using System;
+using System.Collections;
 
-public class Target : MonoBehaviour{
+public class Target : MonoBehaviour
+{
 
     public float health;
     public int numberOfDeathAnimations;
     public float timeTillDestroy;
+    //public float target_DriveMaterial;
+    //public float speed_DriveMaterial = 0.1f;
     private Animator animator;
     private AIStateController controller;
+    private SkinnedMeshRenderer renderer;
+
+    private float shaderValue = 1f;
+    [SerializeField]
+    private float dissolveStartDelayMin = 3f;
+    [SerializeField]
+    private float dissolveStartDelayMax = 3f;
+    [SerializeField]
+    private float change = 0.1f;
+    [SerializeField]
+    private float dissolveDelay = 0.2f;
 
     public void Awake()
     {
         //numberOfDeathAnimations -= 1;
         animator = GetComponent<Animator>();
         controller = GetComponent<AIStateController>();
+        renderer = GetComponentInChildren<SkinnedMeshRenderer>();
     }
 
     public void Hit(float damage){
@@ -37,7 +53,11 @@ public class Target : MonoBehaviour{
         Destroy(GetComponent<CapsuleCollider>());
         Destroy(GetComponent<AIStateController>());
         Destroy(GetComponent<UnityEngine.AI.NavMeshAgent>());
+
         Destroy(this.gameObject, timeTillDestroy);
+        //renderer.material.SetFloat("_DriveMaterial", 0.5f);
+        StartCoroutine(ChangeShader());
+
     }
 
     public int getRandomInt(int numberOfDeathAnimations)
@@ -46,6 +66,45 @@ public class Target : MonoBehaviour{
         int number = rnd.Next(0, numberOfDeathAnimations);
         Debug.Log("Death Anim num: " + number);
         return number;
+    }
+
+    public void dissolveEnemy()
+    {
+        // var d_current_float = renderer.material.GetFloat("_DriveMaterial");
+        // var d_current_float = d_start_float;
+
+        // while(d_current_float < target_DriveMaterial)
+        // {
+        //     renderer.material.SetFloat("_DriveMaterial", d_current_float);
+        //     d_current_float += speed_DriveMaterial;
+        // }
+
+        // Mathf.Lerp(renderer.material.GetFloat("_DriveMaterial"), renderer.material.SetFloat("_DriveMaterial", d_current_float), 0.5);
+    }
+
+    public IEnumerator ChangeShader()
+    {
+
+        shaderValue = 0f;
+        
+        yield return new WaitForSeconds(UnityEngine.Random.Range(dissolveStartDelayMin, dissolveStartDelayMax));
+        //yield return new WaitForSeconds(NextFloat(dissolveStartDelayMin, dissolveStartDelayMin));
+
+        while (shaderValue < 1f){
+
+            yield return new WaitForSeconds(dissolveDelay);             
+
+            //if (this.GetComponentInChildren<SkinnedMeshRenderer>().material = changeDisolve) {
+            shaderValue += change;
+            renderer.material.SetFloat("_DriveMaterial", shaderValue);
+            //}
+        }
+    }
+
+    public float NextFloat(float min, float max){
+        System.Random random = new System.Random();
+        double val = (random.NextDouble() * (max - min) + min);
+        return (float)val;
     }
     
 }
